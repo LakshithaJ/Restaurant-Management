c9ed84c1274868ab38fa58df3c8bdf9e7d765b9b
LakshithaRJ <lakshrj139@gmail.com>
2024-05-20 18:00:16 -0500

    add Add Restaurant and Update Restaurant

---
+++ src/components/AddRestaurant.vue
@@ -0,0 +1,19 @@
+<template>
+    <Header />
+    <h1>Welcome to Add Restaurant Component!</h1>
+</template>
+<script>
+import Header from './Header.vue';
+export default {
+    name: 'AddRestaurant',
+    components : {Header},
+    mounted(){
+        let user = localStorage.getItem('user_info');
+        if(!user){
+            this.$router.push('/signup')
+        }
+    }
+}
+</script>
+<style>
+</style>
---
+++ src/components/UpdateRestaurant.vue
@@ -0,0 +1,19 @@
+<template>
+    <Header />
+    <h1>Welcome to Update Restaurant Component!</h1>
+</template>
+<script>
+import Header from './Header.vue';
+export default {
+    name: 'UpdateRestaurant',
+    components : {Header},
+    mounted(){
+        let user = localStorage.getItem('user_info');
+        if(!user){
+            this.$router.push('/signup')
+        }
+    }
+}
+</script>
+<style>
+</style>
