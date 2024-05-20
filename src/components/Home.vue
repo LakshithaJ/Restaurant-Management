b2ebcb6f305e709c37b49384dcc956293e58e4b2
LakshithaRJ <lakshrj139@gmail.com>
2024-05-20 18:00:40 -0500

    display username

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,16 +1,24 @@
 <template>
     <Header />
-    <h1>Welcome to Home Component!</h1>
+    <h1>Welcome to Home Component {{name.toUpperCase()}}!</h1>
 </template>
 <script>
 import Header from './Header.vue';
 export default {
     name: 'Home',
+    data(){
+        return{
+        name : ''
+        }
+    },
     components : {Header},
     mounted(){
         let user = localStorage.getItem('user_info');
+        
         if(!user){
             this.$router.push('/signup')
+        }else {
+            this.name = JSON.parse(user).name;
         }
     }
 }
